71a5135ac4d713a1c9abbf38d9fdadefd82fe525
Mohammed Adel Mohammed <107713170+MoAdelEzz@users.noreply.github.com>
2023-09-09 15:28:02 +0300

    sus antivirus

--- totally_not_a_hecked_anti_virus/my_territory_my_rules.py
+++ totally_not_a_hecked_anti_virus/my_territory_my_rules.py
@@ -1,6 +1,16 @@
 import psutil
 import pathlib
 import re
+import time
+import keyboard
+from threading import Thread,Lock
+import string
+import os
+import signal
+import shutil
+
+Scanned_Python = dict()
+Scanned_Exes = dict()
 
 def get_process_initial_directory(process):
     try:
@@ -13,37 +23,140 @@ def get_running_python_scripts():
     for proc in psutil.process_iter(['name', 'cmdline']):
         try:
             process_info = proc.as_dict(attrs=['name', 'cmdline'])
-            if process_info['name'] == 'python.exe' and len(process_info['cmdline']) > 1:
+            if  "python" in process_info['name'] and len(process_info['cmdline']) > 1:
                 process = psutil.Process(proc.pid)
                 initial_directory = get_process_initial_directory(process)
-
+                
                 same_file = str(pathlib.Path(process_info['cmdline'][1])) == str(pathlib.Path(__file__).parts[-1])
 
+                pid = proc.pid
+                name = proc.name
                 if initial_directory and not same_file:
-                    python_scripts.append((process_info['cmdline'][1], initial_directory))
+                    python_scripts.append((process_info['cmdline'][1], initial_directory,pid))
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
     return python_scripts
 
+def get_running_exes():
+    exes = []
+    for proc in psutil.process_iter(['name', 'exe']):
+        try:
+            process_info = proc.as_dict(attrs=['name', 'exe'])
+            process = psutil.Process(proc.pid)
+            if process_info['exe'] and not "SYSTEM" in proc.username():
+                pid = proc.pid
+                name = proc.name()
+                exes.append((proc.name(),process_info['exe'],pid))
 
-def scan_python_file(file_path, file_name):
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return exes
+
+def scan_file(file_path, file_name,pid):
+    global Scanned_Python
     full_path = pathlib.Path(file_path).joinpath(file_name)
-    #print(full_path)
+
+    if full_path in Scanned_Python:
+        if Scanned_Python[full_path] == True:
+            try:
+                os.kill(pid,signal.SIGTERM)
+            except Exception as e:
+                pass
+        return
+    
     with open(full_path) as py_file:
         strings = ""
         for line in py_file: strings += line
 
-        pattern = r"keyboard\.on_press"
+        pattern = r"keyboard\.on_press||psutil||keyboard||msvcrt"
         if re.findall(pattern,strings):
-            print(str(full_path) + " made by osama...")
+            print(str(full_path) + " maybe a malware")
+            try:
+                os.kill(pid,signal.SIGTERM)
+                shutil.copyfile(file_path,"./VirtualRecycle/hack" + str(pid) + ".exe")
+                Scanned_Python[full_path] = True
+            except Exception as e:
+                pass
         else:
-            print(str(full_path) + " is 100% secure :pepehappy:")
+            Scanned_Python[full_path] = False
+            
+            
+
+def strings(file_name,file_path,pid, min=4):
+    global Scanned_Exes
     
+    if file_path in Scanned_Exes:
+        if Scanned_Exes[file_path] == True:
+            try:
+                os.kill(pid,signal.SIGTERM)
+            except Exception as e:
+                pass
+        return
 
 
-    
+    strings = []
+    if not os.path.exists(file_path):
+        return False
+    with open(file_path, errors="ignore") as f:
+        result = ""
+        for c in f.read():
+            if c in string.printable:
+                result += c
+                continue
+            if len(result) >= min and result in ["keyboard","pynput","mouse"]:
+                print(file_path + " maybe a malware")
+                try:
+                    os.kill(pid,signal.SIGTERM)
+                    shutil.copyfile(file_path,"./VirtualRecycle/hack" + str(pid) + ".exe")
+                    Scanned_Exes[file_path] = True
+                except Exception as e:
+                    pass
+                return True
 
-running_scripts = get_running_python_scripts()
+            result = ""
+        if len(result) >= min and result in ["keyboard","pynput","mouse"]:  # catch result at EOF
+            print(file_path + " maybe a malware")
+            try:
+                os.kill(pid,signal.SIGTERM)
+                shutil.copyfile(file_path,"./VirtualRecycle/hack" + str(pid) + ".exe")
+                Scanned_Exes[file_path] = True
+            except Exception as e:
+                pass
+            return True
+    Scanned_Exes[file_path] = False
+    return False
 
-for [script_path, initial_directory] in running_scripts:
-    scan_python_file(initial_directory,script_path)
+terminate = False    
+
+def python_thread():
+    global terminate
+    while True:
+        running_scripts = get_running_python_scripts()
+        for [script_path, initial_directory,pid] in running_scripts:
+            scan_file(initial_directory,script_path,pid)
+        time.sleep(1)
+        if terminate:
+            return
+
+def exe_thread():
+    global terminate
+    while True:
+        exes = get_running_exes()
+        for [file_name,file_path,pid] in exes:
+            strings(file_name,file_path,pid)
+        time.sleep(1)
+        if terminate:
+            return
+python = Thread(target=python_thread)
+exe = Thread(target=exe_thread)
+
+
+python.start()
+exe.start()
+
+while str(input()) != "0":
+    continue
+terminate = True
+
+python.join()
+exe.join()
--- totally_not_a_hecked_anti_virus/totally_not_a_heck.py
+++ totally_not_a_hecked_anti_virus/totally_not_a_heck.py
@@ -1,6 +1,17 @@
-import sys
-import time
-x = 0
-while True:
-    print(x)
-    time.sleep(1)
+import string
+
+def strings(filename, min=4):
+    strings = []
+    with open(filename, errors="ignore") as f:  # Python 3.x
+    # with open(filename, "rb") as f:           # Python 2.x
+        result = ""
+        for c in f.read():
+            if c in string.printable:
+                result += c
+                continue
+            if len(result) >= min:
+                strings.append(result)
+            result = ""
+        if len(result) >= min:  # catch result at EOF
+            strings.append(result)
+    return strings
